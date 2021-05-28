e969c15900db036d1ab3f7836646304a11096b92
David Bailey <davidbailey00@outlook.com>
2021-05-28 11:23:13 +0100

    Create explicit spinning animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,31 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount = 0.0
 
     var body: some View {
-        print(animationAmount)
-
-        return VStack {
-            Stepper(
-                "Scale amount",
-                value: $animationAmount.animation(
-                    .easeInOut(duration: 0.5)
-                        .repeatCount(3, autoreverses: true)
-                ),
-                in: 1 ... 10
-            )
-            Spacer()
-            Button("Tap Me") {
-                self.animationAmount += 1
+        Button("Tap me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
             }
-            .padding(40)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
         }
-        .padding()
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0, y: 1, z: 0)
+        )
     }
 }
 
