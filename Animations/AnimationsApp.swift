a37bc140e485d8b1794bffd4d4bf52be88b0c26d
David Bailey <davidbailey00@outlook.com>
2021-05-28 10:48:06 +0100

    initial commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by David Bailey on 28/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
